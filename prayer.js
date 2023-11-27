52186e241594ac84abc576b69ec354817e6f060b
Cyiza2001 <82471629+Cyiza2001@users.noreply.github.com>
2023-11-27 12:34:03 +0200

    the second commit

--- prayer.js
+++ prayer.js
@@ -1,14 +1,16 @@
-let verses = JSON.parse(localStorage.getItem('verses')) ||[];
-console.log(verses);
- verses.push(prompt('Please add your favorite verse:'));
- localStorage.setItem('verses', JSON.stringify(verses));
+let verses = [localStorage.getItem('myFavoriteVerses')]|| [];
+console.log(typeof verses);
+let addVerse = true;
+if (verses && addVerse){
+    verses.push(prompt('Please add your favorite verse:'));
+
+};
+localStorage.setItem('myFavoriteVerses',verses);
 
 const randomVerseIndex = Math.trunc(Math.random()*verses.length);
+console.log(randomVerseIndex);
 const randomVerse = verses[randomVerseIndex]
 for ( let verse of verses){
    if (verse === randomVerse){
-    console.log(verse)};
-};
-
-
- 
+    alert(`Remember to play cause Satan is willing to make you fall:${verse}`);
+}};
